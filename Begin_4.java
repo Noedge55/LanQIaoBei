20d9f4899edac147ddf26ae5158d3ec7ddf9b3ee
noedge55 <noedge@yeah.net>
2017-05-08 13:11:57 +0800

    蓝桥杯 - 入门训练 - Fibonacci数列 - java - 数组计算(非递归)

---
+++ Begin_4.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Begin_4 {
+    public static void main(String[] args) {    
+        Scanner scanner = new Scanner(System.in) ;
+        int n = scanner.nextInt() ;
+        int mod = 10007 ;
+        int arr[] = new int[1000000] ;
+        arr[1] = 1; arr[2] = 1 ;
+        for(int i = 3 ; i <= n ; i++){
+            arr[i] = (arr[i-1]+arr[i-2])%mod ; //求得arr[n]并取模
+        }
+        System.out.println(arr[n]);
+    }
+}
